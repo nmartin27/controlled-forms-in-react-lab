62bdfd7eac050442f1819e906f54e5530085f5d2
nmartin <nate.martin27@gmail.com>
2024-12-04 18:41:15 -0500

    exercise 3-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
 import './App.css'
-import Bookshelf from './Bookshelf.jsx'
+import Bookshelf from './components/Bookshelf.jsx'
 
 
 
 
 const App = () => {
-  return <h1>My Bookshelf </h1>;
+  return( 
+  <>
+  <h1>My Bookshelf</h1>
   <Bookshelf />
+  </>
+  );
 };
 
 export default App;
--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -11,11 +11,37 @@ const [newBooks, setNewBooks] = useState({
     author: '',
 })
 
+const handleInputChange = (event) => {
+setNewBooks({...newBooks, [event.target.name] : event.target.value})
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    setBooks(`title: ${newBooks.title} Author: ${newBooks.author}`)
+    setNewBooks({title:'', author:''})
+}
+
 return (
     <>
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title: </label>
+        <input id="title"
+        name='title'
+        value={newBooks.title}
+        onChange={handleInputChange} />
+    </form>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="author">Author: </label>
+        <input id="author"
+        name='author'
+        value={newBooks.author}
+        onChange={handleInputChange} />
+        <button type="submit">Submit your Book</button>
+    </form>
+
   </div>
   <div className="bookCardsDiv"></div>
   </div>
